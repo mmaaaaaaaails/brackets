ab7ac6c8f0cff8b1044df6f4db4cb1f5b0c47f14
Vadim <v.dudko93@gmail.com>
2020-03-21 19:34:56 +0300

    feat: solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  const strBrackets = str.split('');
+  for (let i = 0; i < strBrackets.length; i++){
+    for (let j = 0; j < bracketsConfig.length; j++){
+      if (bracketsConfig[j][0] === strBrackets[i] && bracketsConfig[j][1] === strBrackets[i + 1]){
+        strBrackets.splice(i, 2);
+        i = i - 2;
+      }
+    }
+  }
+    return strBrackets.length === 0 ? true : false;
+};
